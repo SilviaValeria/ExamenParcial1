5ed386bfd7d54d84dae4102b4082fadc76d09547
linuxdummy <SilviaValeria>
2026-02-26 22:49:31 +0000

    Agrego pregunta 5

---
+++ pregunta5.sh
@@ -0,0 +1,50 @@
+ 141  cd/etc; ls | grep cron
+  142  cd ~/ExamenParcial1
+  143  nano main.cpp
+  144  g++ -std=c++17 -pthread main.cpp -o examen
+  145  ./examen
+  146  cat salida.txt
+  147  nano main.cpp
+  148  g++ -std=c++17 -pthread main.cpp -o examen
+  149  ./examen
+  150  cat salida.txt
+  151  nano README.md
+  152  git init
+  153  git add .
+  154  git commit -m "Examen Parcial 1 - multithreading a archivo"
+  155  git branch -M main
+  156  https://github.com/SilviaValeria/ExamenParcial1.git
+  157  clean
+  158  clear
+  159  pwd
+  160  /home/linuxdummy/ExamenParcial1
+  161  git init
+  162  git add .
+  163  git commit -m "Examen Parcial 1"
+  164  git remote add origin https://github.com/SilviaValeria/ExamenParcial1.git
+  165  git branch -M main
+  166  git push -u origin main
+  167  git branch -M main
+  168  git push -u origin main
+  169  git remote -v
+  170  clear
+  171  git remote -v
+  172  git remote set-url origin https://github.com/SilviaValeria/ExamenParcial1.git
+  173  git push -u origin main
+  174  git status
+  175  git .
+  176  git add .
+  177  clar
+  178  clear
+  179  xdg-open https://github.com/
+  180  cd ~/ExamenParcial1
+  181  git init
+  182  git add .
+  183  git commit -m "Examen Parcial 1"
+  184  git remote add origin https://github.com/SilviaValeria/ExamenParcial1.git
+  185  branch -M main
+  186  git push -u origin main
+  187  clear
+  188  touch accept.txt; ls > accept.txt; history | grep -o '\bls\b' | wc -l >> accept.txt
+  189  cat accept.txt
+  190  history
